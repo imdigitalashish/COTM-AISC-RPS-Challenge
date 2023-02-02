c768586412f511c4ab421b46a74704b126e4db57
imdigitalashish <imdigitalashish@gmail.com>
2023-02-02 16:49:25 +0530

    Main Scree Completed; Game Logic Done; Server Backend Done

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{window, LogicalSize, PhysicalSize, Size};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +12,13 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // window
+    //     .set_size(Size::Physical(PhysicalSize {
+    //         width: 100,
+    //         height: 100,
+    //     }))
+    //     .unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
